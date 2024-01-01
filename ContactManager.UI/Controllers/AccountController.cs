88a1881870a4efa1825b92cf9acb0d450bb8973d
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-01 23:07:20 +0530

    Modified AccountController to make use of UserManager for the validation checking & storing using Identity

--- ContactManager.UI/Controllers/AccountController.cs
+++ ContactManager.UI/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using ContactManager.Core.DTO;
+﻿using ContactManager.Core.Domain.IdentityEntities;
+using ContactManager.Core.DTO;
 using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactManager.UI.Controllers
@@ -7,16 +9,45 @@ namespace ContactManager.UI.Controllers
     [Route("[controller]/[action]")]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Register(RegisterDTO registerDTO)
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
         {
             //Store User Registration Details into Identity DB
-            return RedirectToAction(nameof(PersonsController.Index),"Persons");
+            if(!ModelState.IsValid)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(x => x.Errors).Select(x=>x.ErrorMessage).ToList();
+            }
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerDTO.Email,
+                UserName = registerDTO.Email,
+                PersonName = registerDTO.PersonName,
+                PhoneNumber = registerDTO.Phone
+            };
+            IdentityResult result = await _userManager.CreateAsync(user);
+            if(result.Succeeded)
+            {
+                return RedirectToAction(nameof(PersonsController.Index), "Persons");
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Register",error.Description);
+                }
+                return View(registerDTO);
+            }
         }
     }
 }
