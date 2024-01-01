27e4c7e722d190e0c56472093147c51bd56c0c5f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-01 22:03:27 +0530

    Created AccountController.cs & written Get, Post methods for Register Action Method

---
+++ ContactManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using ContactManager.Core.DTO;
+using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterDTO registerDTO)
+        {
+            //Store User Registration Details into Identity DB
+            return RedirectToAction(nameof(PersonsController.Index),"Persons");
+        }
+    }
+}
