a128ad8a498a96aab67986e43884da796aea4680
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-02 14:02:42 +0530

    Modified Register Post Action Method logic to check for Admin or User Role & adding the same into Roles,UserRoles Tables

--- ContactManager.UI/Controllers/AccountController.cs
+++ ContactManager.UI/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using ContactManager.Core.Domain.IdentityEntities;
 using ContactManager.Core.DTO;
+using ContactManager.Core.Enums;
 using CRUDExample.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -7,17 +8,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ContactManager.UI.Controllers
 {
-    [Route("[controller]/[action]")]
+    //[Route("[controller]/[action]")] //Uncomment this if we don't choose for conventional routing
     [AllowAnonymous] //all the action methods if this controller are able to open even without login
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
         [HttpGet]
         public IActionResult Register()
@@ -43,6 +46,28 @@ namespace ContactManager.UI.Controllers
             IdentityResult result = await _userManager.CreateAsync(user, registerDTO.Password);
             if (result.Succeeded)
             {
+                if(registerDTO.UserType == UserTypeOptions.Admin)
+                {
+                    //Create 'Admin' role
+                    string adminUserType = UserTypeOptions.Admin.ToString();
+                    var adminUser = await _roleManager.FindByNameAsync(adminUserType);
+                    if(adminUser != null)
+                    {
+                        ApplicationRole applicationRole = new ApplicationRole()
+                        {
+                            Name = adminUserType
+                        };
+                        await _roleManager.CreateAsync(applicationRole); //inserts new row in AspNetRoles table
+                    }
+                    //Add newly created user into 'Admin' role
+                    await _userManager.AddToRoleAsync(user,adminUserType); //inserts new row in AspNetUserRoles table
+                }
+                else
+                {
+                    string userType = UserTypeOptions.User.ToString();
+                    //Add newly created user into 'User' role
+                    await _userManager.AddToRoleAsync(user, userType); //inserts new row in AspNetUserRoles table
+                }
                 //sign-in
                 await _signInManager.SignInAsync(user, isPersistent: false);
                 return RedirectToAction(nameof(PersonsController.Index), "Persons");
